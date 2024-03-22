48b8afa95730a1d375d078ff5901a6cd68eb5935
ziyadanbari <ziyadanbari@gmail.com>
2024-03-22 23:52:40 +0000

    change quality download

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ app.get("/getMusic", async (req, res) => {
 
     const audio = ytdl(videoURL, {
       filter: "audioonly",
-      quality: "highestaudio",
     });
 
     let downloadedBytes = 0;
@@ -89,7 +88,7 @@ app.post("/downloadPlaylist", async (req, res) => {
   for (let name in urls) {
     const url = urls[name];
     const chunks = [];
-    const audio = ytdl(url, { filter: "audio", quality: "highestaudio" });
+    const audio = ytdl(url, { filter: "audio" });
     audio.on("data", (chunk) => {
       chunks.push(chunk);
       downloadedBytes += chunk.length;
