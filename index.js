048a96c6f96c3d8c083a974df9e13022b2c02af5
ziyadanbari <ziyadanbari@gmail.com>
2024-03-25 19:34:19 +0000

    change progressTimeout

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const YD = new YoutubeMp3Downloader({
   ffmpegPath: ffmpegPath, // FFmpeg binary location
   youtubeVideoQuality: "highestaudio", // Desired video quality (default: highestaudio)
   queueParallelism: 2, // Download parallelism (default: 1)
-  progressTimeout: 2000, // Interval in ms for the progress reports (default: 1000)
+  progressTimeout: 500, // Interval in ms for the progress reports (default: 1000)
   allowWebm: false, // Enable download from WebM sources (default: false)
   outputPath: "./audios",
 });
